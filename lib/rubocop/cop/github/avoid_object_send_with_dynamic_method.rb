4f887eeae5876e7b52d3a46d087fa39ce906b2ff
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-01-28 21:09:34 +0200

    dsc

--- lib/rubocop/cop/github/avoid_object_send_with_dynamic_method.rb
+++ lib/rubocop/cop/github/avoid_object_send_with_dynamic_method.rb
@@ -5,3 +5,24 @@ require "rubocop"
 module RuboCop
   module Cop
     module GitHub
+      # Public: A Rubocop to discourage using methods like Object#send that allow you to dynamically call other
+      # methods on a Ruby object, when the method being called is itself completely dynamic. Instead, explicitly call
+      # methods by name.
+      #
+      # Examples:
+      #
+      #     # bad
+      #     foo.send(some_variable)
+      #
+      #     # good
+      #     case some_variable
+      #     when "bar"
+      #       foo.bar
+      #     else
+      #       foo.baz
+      #     end
+      #
+      #     # fine
+      #     foo.send(:bar)
+      #     foo.public_send("some_method")
+      #     foo.__send__("some_#{variable}_method")
