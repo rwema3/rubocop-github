83bafe101ec1703c802096318b22d85d743b0755
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-01-31 22:26:16 +0200

    class Insecure Hash Algorithm

--- lib/rubocop/cop/github/insecure_hash_algorithm.rb
+++ lib/rubocop/cop/github/insecure_hash_algorithm.rb
@@ -5,3 +5,8 @@ require "rubocop"
 module RuboCop
   module Cop
     module GitHub
+      class InsecureHashAlgorithm < Base
+        MSG = "This hash function is not allowed"
+        UUID_V3_MSG = "uuid_v3 uses MD5, which is not allowed"
+        UUID_V5_MSG = "uuid_v5 uses SHA1, which is not allowed"
+
